ec8d25f0f9e0bacb66766946dfe271a9384321e9
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-18 14:04:25 +0530

    Adding script on Fri Nov 18 14:04:25 IST 2016 :airplane_departure,:

--- ecmascript/rna-transcription/babel-output/rna-transcription.js
+++ ecmascript/rna-transcription/babel-output/rna-transcription.js
@@ -16,21 +16,7 @@ var Transcriptor = (function () {
 	_createClass(Transcriptor, [{
 		key: 'toRna',
 		value: function toRna(str) {
-			var str1 = "";
-			for (var i = 0; i < str.length; i++) {
-				switch (str[i]) {
-					case 'C':
-						str1 += 'G';break;
-					case 'G':
-						str1 += 'C';break;
-					case 'A':
-						str1 += 'U';break;
-					case 'T':
-						str1 += 'A';break;
-				}
-			}
-
-			return str1;
+			return str.replace(/C/g, 'G').replace(/G/g, 'C').replace(/A/g, 'U').replace(/T/g, 'A');
 		}
 	}]);
 
